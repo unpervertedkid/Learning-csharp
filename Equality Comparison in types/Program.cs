585a0c86635967afbe7bc6fc71d5568397c5d15b
Brian Silah <unpervertedkid@gmail.com>
2023-02-22 16:33:36 +0300

    Equality comparison in structs

--- Equality Comparison in types/Program.cs
+++ Equality Comparison in types/Program.cs
@@ -15,7 +15,12 @@ public class Program
         
         Console.WriteLine($"First point equals second point: {classPoint == classPoint2}");
         
+        //Comparing structs
+        StructPoint structPoint1 = new StructPoint() { XPoint = 10, YPoint = 10 };
+        StructPoint structPoint2 = structPoint1;
         
+        Console.WriteLine($"First point equals second point: {structPoint1 == structPoint2}");
+
     }
 }
 
--- Equality Comparison in types/StructPoint.cs
+++ Equality Comparison in types/StructPoint.cs
@@ -4,4 +4,7 @@ public struct StructPoint
 {
     public int XPoint { get; set; }
     public int YPoint { get; set; }
+
+    public static bool operator ==(StructPoint a, StructPoint b) => a.XPoint == b.XPoint && a.YPoint == b.YPoint;
+    public static bool operator !=(StructPoint a, StructPoint b) => a.XPoint != b.XPoint && a.YPoint != b.YPoint;
 }
