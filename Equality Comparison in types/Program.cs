45c2a3056e96cc4588d66b57d9cb10d5e0593505
Brian Silah <unpervertedkid@gmail.com>
2023-02-22 16:41:02 +0300

    Record type modification

--- Equality Comparison in types/Program.cs
+++ Equality Comparison in types/Program.cs
@@ -13,13 +13,29 @@ public class Program
 
         ClassPoint classPoint2 = classPoint;
         
-        Console.WriteLine($"First point equals second point: {classPoint == classPoint2}");
+        Console.WriteLine($"First class point equals second class point: {classPoint == classPoint2}");
+
+        classPoint2.YPoint = 20;
+        classPoint2.XPoint = 20;
         
+        Console.WriteLine($"First class point equals second class point after second class point modification: {classPoint == classPoint2}");
+
         //Comparing structs
         StructPoint structPoint1 = new StructPoint() { XPoint = 10, YPoint = 10 };
         StructPoint structPoint2 = structPoint1;
+        StructPoint structPoint3 = new StructPoint() { XPoint = 20, YPoint = 20 };
         
-        Console.WriteLine($"First point equals second point: {structPoint1 == structPoint2}");
+        Console.WriteLine($"First struct point equals second struct point: {structPoint1 == structPoint2}");
+        Console.WriteLine($"First struct point equals third struct point: {structPoint1 == structPoint3}");
+
+        //Comparing records
+        RecordPoint recordPoint1 = new RecordPoint() { XPoint = 10, YPoint = 10 };
+        RecordPoint recordPoint2 = recordPoint1;
+
+        RecordPoint recordPoint3 = new RecordPoint() { XPoint = 20, YPoint = 20 };
+        
+        Console.WriteLine($"First record point equals second record point: {recordPoint1 == recordPoint2}");
+        Console.WriteLine($"First struct point equals third record point: {structPoint1 == structPoint3}");
 
     }
 }
--- Equality Comparison in types/RecordPoint.cs
+++ Equality Comparison in types/RecordPoint.cs
@@ -4,4 +4,7 @@ public struct RecordPoint
 {
     public int XPoint { get; set; }
     public int YPoint { get; set; }
+
+    public static bool operator ==(RecordPoint a, RecordPoint b) => a.XPoint == b.XPoint && a.YPoint == b.YPoint;
+    public static bool operator !=(RecordPoint a, RecordPoint b) => a.XPoint != b.XPoint && a.YPoint != b.YPoint;
 }
