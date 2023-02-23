d2e0cd0f6028c542d7d6e689891d785bc4803c83
Brian Silah <unpervertedkid@gmail.com>
2023-02-23 16:30:26 +0300

    Minor naming convention renaming

--- Static Classes/DatabaseConfig.cs
+++ Static Classes/DatabaseConfig.cs
@@ -4,16 +4,16 @@ public static class DatabaseConfig
 {
     private static string DATABASE_NAME;
     private const int NUMBER_OF_CONNECTIONS = 5;
-    private static string DATABASE_URL;
+    private static readonly string DatabaseUrl;
 
     static DatabaseConfig()
     {
         DATABASE_NAME = "PostgreSql";
-        DATABASE_URL = $"htpps//:{DATABASE_NAME}:4500";
+        DatabaseUrl = $"htpps//:{DATABASE_NAME}:4500";
     }
 
-    public static string getDatabaseUrl()
+    public static string GetDatabaseUrl()
     {
-        return DATABASE_URL;
+        return DatabaseUrl;
     }
 }
--- Static Classes/Program.cs
+++ Static Classes/Program.cs
@@ -4,6 +4,6 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine($"Database url:{DatabaseConfig.getDatabaseUrl()}");
+        Console.WriteLine($"Database url:{DatabaseConfig.GetDatabaseUrl()}");
     }
 }
