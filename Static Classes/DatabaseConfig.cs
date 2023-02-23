513d9a5fd2cc90bd39b09f8945b5ecf06a3837de
Brian Silah <unpervertedkid@gmail.com>
2023-02-23 16:28:50 +0300

    Creating and using static classes

---
+++ Static Classes/DatabaseConfig.cs
@@ -0,0 +1,19 @@
+﻿namespace Static_Classes;
+
+public static class DatabaseConfig
+{
+    private static string DATABASE_NAME;
+    private const int NUMBER_OF_CONNECTIONS = 5;
+    private static string DATABASE_URL;
+
+    static DatabaseConfig()
+    {
+        DATABASE_NAME = "PostgreSql";
+        DATABASE_URL = $"htpps//:{DATABASE_NAME}:4500";
+    }
+
+    public static string getDatabaseUrl()
+    {
+        return DATABASE_URL;
+    }
+}
---
+++ Static Classes/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Static_Classes;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine($"Database url:{DatabaseConfig.getDatabaseUrl()}");
+    }
+}
