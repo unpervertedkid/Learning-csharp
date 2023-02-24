0f071863f6ab708602d99db82524d6ef4a1fc95e
Brian Silah <unpervertedkid@gmail.com>
2023-02-24 19:19:27 +0300

    Declaring variables using types

---
+++ Variables/Program.cs
@@ -0,0 +1,17 @@
+﻿using Abstract_classes;
+
+namespace Variables;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Account account = new CheckingAccount("Brian", "Silah", 2000);
+
+        ((CheckingAccount)account).OverdrawFine = 5.0m;
+        
+        account.Withdraw(2500);
+        
+        Console.WriteLine($"The checking account balance: {account.Balance}");
+    }
+}
