e2165af3b2baa1d0976130edb69acdf4c849fd47
Brian Silah <unpervertedkid@gmail.com>
2023-02-24 19:29:00 +0300

    Using var keyword to declare anonymous types on the fly and use them

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -9,9 +9,14 @@ public class Program
         Account account = new CheckingAccount("Brian", "Silah", 2000);
 
         ((CheckingAccount)account).OverdrawFine = 5.0m;
-        
         account.Withdraw(2500);
         
         Console.WriteLine($"The checking account balance: {account.Balance}");
+
+        var undefinedAccount = new { FirstName = "Ilhan", SecondName = "Okeyo", AccountType = "Undefined" };
+        Console.WriteLine(
+            $"Account for user: {undefinedAccount.FirstName + " " + undefinedAccount.SecondName} is {undefinedAccount.AccountType}.");
+        
+        
     }
 }
