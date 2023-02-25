f81d1e633aee586843c922424131afa3f3db69b6
Brian Silah <unpervertedkid@gmail.com>
2023-02-25 16:36:42 +0300

    Shift employee that has the ability to work on specific shifts

---
+++ Working with classes structs and records/ShiftEmployee.cs
@@ -0,0 +1,11 @@
+﻿namespace Working_with_classes_structs_and_records;
+
+public class ShiftEmployee:Employee
+{
+    public ShiftDays DaysAvailable { get; set; }
+
+    public ShiftEmployee(string firstName, string lastName, int employeeId = 0,ShiftDays shiftDays = ShiftDays.Weekdays): base(firstName,lastName,employeeId)
+    {
+        
+    }
+}
