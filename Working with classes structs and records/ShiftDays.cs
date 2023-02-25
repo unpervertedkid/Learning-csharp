213fe885085445076fd2161af4f43bdab88c820e
Brian Silah <unpervertedkid@gmail.com>
2023-02-25 16:23:40 +0300

    Declaring enums

---
+++ Working with classes structs and records/ShiftDays.cs
@@ -0,0 +1,13 @@
+﻿namespace Working_with_classes_structs_and_records;
+
+public enum ShiftDays
+{
+    Sunday = 1,
+    Monday = 2,
+    Tuesday = 4,
+    Wednesday = 8,
+    Thursday = 16,
+    Friday = 32,
+    Saturday = 64
+    
+}
