99b65c061b5e60b5285d06deb9444b7d5b3e4a48
Brian Silah <unpervertedkid@gmail.com>
2023-02-25 17:08:25 +0300

    Fixed the initialization of a default working days for a shift employee

--- Working with classes structs and records/ShiftEmployee.cs
+++ Working with classes structs and records/ShiftEmployee.cs
@@ -4,8 +4,8 @@ public class ShiftEmployee:Employee
 {
     public ShiftDays DaysAvailable { get; set; }
 
-    public ShiftEmployee(string firstName, string lastName, int employeeId = 0,ShiftDays shiftDays = ShiftDays.Weekdays): base(firstName,lastName,employeeId)
+    public ShiftEmployee(string firstName, string lastName, int employeeId = 0,ShiftDays daysAvailable = ShiftDays.Weekdays): base(firstName,lastName,employeeId)
     {
-        
+        DaysAvailable = daysAvailable;
     }
 }
