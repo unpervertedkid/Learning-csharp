de9cd214735e591bc8e8ce27076564ae7946ffb4
Brian Silah <unpervertedkid@gmail.com>
2023-02-26 10:27:08 +0300

    Working with nullables

--- Working with nullables/Program.cs
+++ Working with nullables/Program.cs
@@ -7,6 +7,16 @@ public class Program
         string name = null;
         int? age = null;
 
+        int definitiveInt = age ?? 17;
+        Console.WriteLine($"Definitive int: {definitiveInt}");
+            
+        age = 5;
+        definitiveInt = age ?? 17;
+        Console.WriteLine($"Definitive int: {definitiveInt}");
+
+        definitiveInt = age != null ? age.Value : 17;
+        Console.WriteLine($"Definitive int: {definitiveInt}");
+
         if (name != null)
         {
             Console.WriteLine($"Name is {name}");
