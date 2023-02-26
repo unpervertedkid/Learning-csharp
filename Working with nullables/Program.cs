d8512cc54fd5fb4e4105d5b4cfe30bf147c8482f
Brian Silah <unpervertedkid@gmail.com>
2023-02-26 10:15:13 +0300

    Defining nullables, Strings can be null but ints cannot by default

---
+++ Working with nullables/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Working_with_nullables;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string name = null;
+        int? age = null;
+
+        if (name != null)
+        {
+            Console.WriteLine($"Name is {name}");
+        }
+
+        if (age != null)
+        {
+            Console.WriteLine($"Age is {age}");
+        }
+    }
+}
