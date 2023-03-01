8fd690315a57e7ae220844c763b001c53921d6c5
Brian Silah <unpervertedkid@gmail.com>
2023-03-01 17:13:44 +0300

    Test to test that adding the same item several times to the cat updates the quantity of the item appropriately

--- HPlusTDD.Core.Tests/ShoppingCartTests.cs
+++ HPlusTDD.Core.Tests/ShoppingCartTests.cs
@@ -45,23 +45,55 @@ public class ShoppingCartTests
             Quantity = 10
         };
 
-        var request = new AddToCartRequest()
+        var request1 = new AddToCartRequest()
         {
             Item = item1
         };
 
-        request = new AddToCartRequest()
+        var request2 = new AddToCartRequest()
         {
             Item = item2;
         };
 
         var manager = new ShoppingCartManager();
 
-        AddToCartResponse response = manager.AddToCart(request);
+        AddToCartResponse response = manager.AddToCart(request1);
+        response = manager.AddToCart(request2);
 
         Assert.NotNull(response);
         Assert.Contains(item1, response.Items);
         Assert.Contains(item2, response.Items);
 
+    }
+
+    [Test]
+    public void ShouldUpdateQuantityWhenArticleIsAddedMultipleTimes()
+    {
+        var item1 = new AddToCartItem()
+        {
+            ArticleId = 20,
+            Quantity = 5
+        };
+        var item2 = new AddToCartItem()
+        {
+            ArticleId = 20,
+            Quantity = 10
+        };
+
+        var request1 = new AddToCartRequest()
+        {
+            Item = item1
+        };
+
+        var request2 = new AddToCartRequest()
+        {
+            Item = item2
+        };
+
+        var manager = new ShoppingCartManager();
+        var response = manager.AddToCart(request1);
+        response = manager.AddToCart(request2);
+
+        Assert.That(response.Items[0].Quantity == 15);
     }
 }
