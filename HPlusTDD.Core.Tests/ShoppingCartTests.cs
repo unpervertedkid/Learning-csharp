1a7392123906992a0b7793d11d0bc0758456719d
Brian Silah <unpervertedkid@gmail.com>
2023-03-01 16:55:16 +0300

    Added a test to test adding multiple items/articles to the cart.

--- HPlusTDD.Core.Tests/ShoppingCartTests.cs
+++ HPlusTDD.Core.Tests/ShoppingCartTests.cs
@@ -30,4 +30,38 @@ public class ShoppingCartTests
         Assert.Contains(item, response);
 
     }
+    
+    [Test]
+    public void ShouldReturnArticlesAddedToCart()
+    {
+        var item1 = new AddToCartItem()
+        {
+            ArticleId = 42,
+            Quantity = 5
+        };
+        var item2 = new AddToCartItem()
+        {
+            ArticleId = 43,
+            Quantity = 10
+        };
+
+        var request = new AddToCartRequest()
+        {
+            Item = item1
+        };
+
+        request = new AddToCartRequest()
+        {
+            Item = item2;
+        };
+
+        var manager = new ShoppingCartManager();
+
+        AddToCartResponse response = manager.AddToCart(request);
+
+        Assert.NotNull(response);
+        Assert.Contains(item1, response.Items);
+        Assert.Contains(item2, response.Items);
+
+    }
 }
